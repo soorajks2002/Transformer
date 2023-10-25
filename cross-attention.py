ad62ce9a10df7811360ef51acc45d5bb9b33f608
sooraj <soorajks2002@gmail.com>
2023-10-25 14:01:37 +0530

    added residual connection & layer normalization

--- cross-attention.py
+++ cross-attention.py
@@ -4,7 +4,13 @@ class CrossAttention(torch.nn.Module) :
     def __init__(self, embedding_size, n_attention_heads) :
         super(CrossAttention, self).__init__()
         self.attention = torch.nn.MultiheadAttention(embedding_size, n_attention_heads)
+        self.layer_normalization = torch.nn.LayerNorm(embedding_size)
         
     def __call__(self, encoder_x, decoder_x) :
-        out,_ = self.attention(decoder_x, encoder_x, encoder_x)
-        return out
+        output,_ = self.attention(decoder_x, encoder_x, encoder_x)
+        output = output.view(-1, output.size(2))
+        output = self.layer_normalization(output)
+        output = output.view(decoder_x.size(0), decoder_x.size(1), decoder_x.size(2))
+        output += decoder_x
+        
+        return output
--- masked-multi-head-attention.py
+++ masked-multi-head-attention.py
@@ -1,15 +1,22 @@
 import torch
 
-class MultiHeadAttention(torch.nn.Module) :
+class MaskedMultiHeadAttention(torch.nn.Module) :
     def __init__(self, embedding_size, n_attention_heads) :
-        super(MultiHeadAttention, self).__init__()
+        super(MaskedMultiHeadAttention, self).__init__()
         self.attention = torch.nn.MultiheadAttention(embedding_size, n_attention_heads)
-    
+        self.layer_normalization = torch.nn.LayerNorm(embedding_size)
+        
     def __call__(self, x) :
-        x = x.permute(1,0,2)
+        output = x.permute(1,0,2)
         
-        mask = torch.tril(torch.ones(x.size(0), x.size(0)), diagonal=0)
+        mask = torch.tril(torch.ones(output.size(0), output.size(0)), diagonal=0)
         mask = mask.masked_fill(mask == 0, float('-inf'))
-        output, _ = self.attention(x, x, x, attn_mask=mask)
+        output, _ = self.attention(output, output, output, attn_mask=mask)
+        output = output.permute(1,0,2)
         
-        return output.permute(1,0,2)
+        output = output.reshape(-1, output.size(2))
+        output = self.layer_normalization(output)
+        output = output.view(x.size(0), x.size(1), x.size(2))
+        output += x
+        
+        return output
